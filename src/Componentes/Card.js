d3b73f2a8e4c31cc257c52e997676b154c146865
Thales Spanhol <thales_spanhol@hotmail.com>
2023-04-18 17:20:47 -0300

    Feat: filtro por tipo

--- src/Componentes/Card.js
+++ src/Componentes/Card.js
@@ -7,7 +7,7 @@ const Card = ({name, sprite, types, selectPokemon, pokemon}) => {
           <img alt={name} src={sprite}/>
         </Link>
         <h2>{name[0].toUpperCase() + name.substring(1)}</h2>
-        {types.map((e) => <h3>{e.type.name[0].toUpperCase() + e.type.name.substring(1)}</h3>)}
+        {types.map((e) => <h3 key={e.type.name}>{e.type.name[0].toUpperCase() + e.type.name.substring(1)}</h3>)}
       </div>
   );
 }
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -10,6 +10,7 @@ function Home(props) {
   const API = "https://pokeapi.co/api/v2/pokemon?limit=151";
   const [page, setPage] = useState([0,20]);
   const [search, setSearch] = useState('');
+  const [sort, setSort] = useState('All');
   const types = [{type: 'Normal', number: 1}, {type: 'Fighting', number: 2}, {type: 'Flying', number: 3}, {type: 'Poison', number: 4}, {type: 'Ground', number: 5}, {type: 'Rock', number: 6}, {type: 'Bug', number: 7}, {type: 'Ghost', number: 8}, {type: 'Steel', number: 9}, {type: 'Fire', number: 10}, {type: 'Water', number: 11}, {type: 'Grass', number: 12}, {type: 'Electric', number: 13}, {type: 'Psychic', number: 14}, {type: 'Ice', number: 15}, {type: 'Dragon', number: 16}, {type: 'Dark', number: 17}, {type: 'Fairy', number: 18}];
 
   const filtro = (string) => {
@@ -30,9 +31,18 @@ function Home(props) {
 
   const selectPokemon = (param) => dispatch(setPokemon(param));
 
+  const noPokemon = () => {
+    if (props.pokedex && props.pokedex.slice(page[0],page[1]).filter((e) => e.data.name.startsWith(search)).length === 0) {
+      return <h1>Nenhum Pokemon Encontrado</h1>;
+    }
+  }
+
+
   useEffect(() => {
     getPokemons(API);
   }, []);
+
+
 
   return (
     <div>
@@ -46,21 +56,37 @@ function Home(props) {
           }
         }}/>
       </div>
-      {types.map((e) => <button onClick={() => getPokemons(`https://pokeapi.co/api/v2/type/${e.number}`)}>{e.type}</button>)}
+      {types.map((e) => <button key={`${e.type}-button`} onClick={() => {
+        if (sort === e.type) {
+          getPokemons(API);
+          setSort('All');
+          setPage([0,20]);
+        } else {
+          getPokemons(`https://pokeapi.co/api/v2/type/${e.number}`);
+          setSort(e.type);
+          setPage([0,20]);
+        }
+      }}>{e.type}</button>)}
 
       {props.pokedex ? (
-        props.pokedex.slice(page[0],page[1]).map((e) => e.data.name.startsWith(search) && <Card name={e.data.name} sprite={Object.values(e.data.sprites.other)[2].front_default} selectPokemon={selectPokemon} pokemon={e.data} types={e.data.types} />)
+        props.pokedex.slice(page[0],page[1]).map((e) => e.data.name.startsWith(search.toLowerCase()) && <Card key={e.data.name} name={e.data.name} sprite={Object.values(e.data.sprites.other)[2].front_default} selectPokemon={selectPokemon} pokemon={e.data} types={e.data.types} />)
       ) : (
         <h1>Loading</h1>
       )}
+
+      {/* {props.pokedex && props.pokedex.slice(page[0],page[1]).filter((e) => e.data.name.startsWith(search)).length === 0 && <h1>Nenhum Pokemon Encontrado</h1>} */}
+
+      {noPokemon()}
+
 
       <div><button disabled={page[0] === 0} onClick={() => {
         if (page[0] >= 20) {
           setPage([page[0] -20, page[1] -20])
         }
-      }}>{'<'}</button><button disabled={page[1] === 160} onClick={() => {
-        if (page[1] <= 140) {
+      }}>{'<'}</button><button disabled={page[1] >= props.pokedex.length} onClick={() => {
+        if (page[1] < props.pokedex.length) {
           setPage([page[0] +20, page[1] +20])
+          console.log(props.pokedex.length);
         }
       }}>{'>'}</button></div>
     </div>
